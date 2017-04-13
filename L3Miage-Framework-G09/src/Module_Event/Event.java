b71dc3f499a3b10969b928c2abaf2b0558a3d2b9
ramadanmax <maxime.ramadanoski@gmail.com>
2017-04-13 10:25:35 +0200

    Commit Event

---
+++ L3Miage-Framework-G09/src/Module_Event/Event.java
@@ -0,0 +1,12 @@
+package Module_Event;
+import java.util.Date;
+import java.util.List;
+
+public class Event{
+private String nom,lieu;
+private Date date;
+private List<Contact> contacts;
+	public Event(){
+	
+	}
+}
